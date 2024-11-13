7d94bfc91cdc321c01cbb73d7f67aaeb0443c48a
Yoon Me Me <yoonmeme.engr@gmail.com>
2024-11-13 13:57:30 +0630

    first commit

--- js/script.js
+++ js/script.js
@@ -4,17 +4,60 @@ new WOW().init();
 // Spinner Start
 window.addEventListener("load", () => {
   document.querySelector(".lds-ring").style.display = "none"; 
-})
+});
 // Spinner End
 
 // Navbar Start
+// $(window).scroll(function () {
+//   if($(this).scrollTop() > 300) {
+//     $('.navbar').fadeIn('slow').css('display', 'flex');
+//   } else {
+//     $('.navbar').fadeIn('slow').css('display', 'none');
+//   }
+// })
+
 $(window).scroll(function () {
-  if($(this).scrollTop() > 300) {
-    $('.navbar').fadeIn('slow').css('display', 'flex');
+  if ($(this).scrollTop() > 300) {
+      $('.navbar').fadeIn('slow').css('display', 'flex');
   } else {
-    $('.navbar').fadeIn('slow').css('display', 'none');
+      $('.navbar').fadeOut('slow').css('display', 'none');
   }
-})
+});
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  // Browser ကို Scroll ဆွဲလိုက်တဲ့အခါ Scroll ရောက်နေတဲ့ Height ကိုရရှိနေမှာဖြစ်ပါတယ်။
+  const scrollY = window.pageYOffset;           // scroll height
+
+  // forEach နဲ့ ရှိသမျှ section အကုန်လုံးကို loop လုပ်လိုက်တာဖြစ်ပါတယ်။
+  // current.offsetHeight လက်ရှိရောက်နေတဲ့ section ရဲ့ Height ကို ရယူလိုက်တာပါ။
+  // current.offsetTop - 58 လက်ရှိရောက်နေတဲ့ section ရဲ့ Top ကို ရယူလိုက်တာပါ။
+  // current.getAttribute("id") လက်ရှိရောက်နေတဲ့ section ရဲ့ id ကို ရယူလိုက်တာပါ။
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight, // get current height
+      sectionTop = current.offsetTop - 58,      // get current section of height
+      sectionId = current.getAttribute("id");   // get current section id
+
+    // scrollyY သည် sectionTop ထက်ကြီးနေတယ်ဆိုရင် True အနေနဲ့ သတ်မှတ်မှာဖြစ်ပြီး
+    // လက်ရှိရောက်နေတဲ့ section ရဲ့ Top နဲ့ လက်ရှိရောက်နေတဲ့ section ရဲ့ Height နှစ်ခုကိုပေါင်းပြီး
+    // scrollyY သည် ငယ်နေတယ်(သို့) ညီနေတယ်ဆိုရင် True ကိုရရှိမှာပါ။
+    // && သည် Comparsion Operator ဖြစ်ပြီး နှင်းယှဉ်ထားတဲ့ တန်ဖိုးနှစ်ခုလုံး True && True ဖြစ်မှသာ
+    // if statement ကိုအလုပ်လုပ်မှာဖြစ်ပါတယ်။ နှိုင်းယှဉ်လိုက်တဲ့ တန်ဖိုးက false ဖြစ်နေလျှင် else ကို အလုပ်လုပ်သွားမှာဖြစ်ပါတယ်။
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { // <= Less than or equal to
+      document
+        .querySelector("a[href*=" + sectionId + "]").classList.add("active-link");
+    } else {
+      document
+        .querySelector("a[href*=" + sectionId + "]").classList.remove("active-link");
+    }
+  });
+}
+// function သည် ကြေညာလိုက်ရုံနဲ့ အလုပ်မလုပ်ပါဘူး။
+// သူ့ကို ပြန်ခေါ်သုံးမှသာ အလုပ်လုပ်တာဖြစ်ပါတယ်။
+// ဒါကြောင့် Browser ကို Scroll လုပ်လိုက်လျှင် scrollActive ကို အလုပ်လုပ်ပေးပါလို့ပြောလိုက်ခြင်းဖြစ်ပါတယ်။
+window.addEventListener("scroll", scrollActive);
+
 // Navbar End
 
 // Home Start
@@ -70,33 +113,32 @@ $(".testimonial-carousel").owlCarousel({
 // Let's Work Together End
 
 // Footer Start
-const changeTheme = document.getElementById("changeTheme");
+var darkMode = false;
 
-changeTheme.addEventListener("change", () => {
-  const theme = localStorage.getItem("data-theme");
-  if (theme === "dark") {
-    changeToLight();
-  } else {
-    changeToDark();
-  }
-});
+// default to system setting
+// if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+// 	darkMode = true;
+// }
 
-const theme = localStorage.getItem("data-theme");
-if (theme === "dark") {
-  changeToDark();
-} else {
-  changeToLight();
-}
+// // preference from localStorage should overwrite
+// if (localStorage.getItem('theme') === 'dark') {
+// 	darkMode = true;
+// } else if (localStorage.getItem('theme') === 'light') {
+// 	darkMode = false;
+// }
 
-const changeToDark = () => {
-  document.documentElement.setAttribute("data-theme", "dark");
-  localStorage.setItem("data-theme", "dark")
-}
+// if (darkMode) {
+// 	document.body.classList.toggle('dark');
+// }
 
-const changeToLight = () => {
-  document.documentElement.setAttribute("data-theme", "light");
-  localStorage.setItem("data-theme", "light")
-}
+// document.addEventListener('DOMContentLoaded', () => {
+
+//     document.getElementById('theme-toggle').addEventListener('click', () => {
+// 		document.body.classList.toggle('dark');
+//     	localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
+// 	});
+
+// });
 // Footer End
 
 // Back to Top Start
